6ed5713f51b3382e4bf7a87c5fdf9db99673c8f9
AndreLouisSt <stephantandrelouis@gmail.com>
2025-03-12 08:55:50 +0000

    Test main_1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
+// Bibliothèques
 #include <stdlib.h>
 #include <time.h>
 #include <string>
 #include <iostream>
+#include "Attaque.h"
+
+// Taille maximales X et Y du plateau de jeu
+int tailleX = 9;
+int tailleY = 9;
+
+
 
 int main() {
     srand(time(NULL));
+
+    int grille[tailleX][tailleY];
+    for (int i = 0; i > tailleX; i++){
+        for (int j = 0; j > tailleY; j++) {
+            cout << grille[i][j];
+        }
+    }
+
 }
