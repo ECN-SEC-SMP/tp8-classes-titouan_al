16c4c7cd0ab9c5d09a07d7987c82b9a632ff1775
AndreLouisSt <stephantandrelouis@gmail.com>
2025-03-12 20:25:58 +0000

    Correction ours.cpp

--- Ours.cpp
+++ Ours.cpp
@@ -9,36 +9,13 @@
 // Déplacement 8 directions basé sur des vecteurs positions
 void Ours::deplace(int maxX, int maxY)
 {
+    const int deplaceX[] = {2, 1, -1, -2, -2, -1, 1, 2};
+    const int deplaceY[] = {1, 2, 2, 1, -1, -2, -2, -1};
     int a = rand() % 8;
-    switch (a)
-    {
-    case 1:
-        this->x = (this->x - 1) % maxX;
-        this->y = (this->y - 2) % maxY;
-    case 2:
-        this->x = (this->x - 2) % maxX;
-        this->y = (this->y - 1) % maxY;
-    case 3:
-        this->x = (this->x - 2) % maxX;
-        this->y = (this->y + 1) % maxY;
-    case 4:
-        this->x = (this->x - 1) % maxX;
-        this->y = (this->y + 2) % maxY;
-    case 5:
-        this->x = (this->x + 1) % maxX;
-        this->y = (this->y + 2) % maxY;
-    case 6:
-        this->x = (this->x + 2) % maxX;
-        this->y = (this->y + 1) % maxY;
-    case 7:
-        this->x = (this->x + 2) % maxX;
-        this->y = (this->y - 1) % maxY;
-    case 8:
-        this->x = (this->x + 1) % maxX;
-        this->y = (this->y - 2) % maxY;
-    default:
-        break;
-    }
+
+    // Modification de la position en fct de a (tableaux de valeurs), contraint avec la taille max. du tableau
+    x = (x + deplaceX[a])%maxX;
+    y = (y + deplaceY[a])%maxY;
 }
 
 // On récupère la valeur de l'attaque (0, 1 ou 2)
