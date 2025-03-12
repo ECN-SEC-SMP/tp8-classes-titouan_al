08eb019e44ad932ba31faec0b317b029eb53db07
AndreLouisSt <stephantandrelouis@gmail.com>
2025-03-12 08:26:35 +0000

    Création attaque_3

--- Attaque.cpp
+++ Attaque.cpp
@@ -1,27 +1,57 @@
+// Bibliothèques
 #include <string>
 #include <iostream>
 #include "stdlib.h"
 #include "Attaque.h"
 
+// Création attaque aléatoire (0, 1 ou 2)
 Attaque::Attaque()
 {
-    rand()%2;
+    this->type = rand() % 2;
 }
 
+// C
 Attaque::Attaque(int a)
 {
-    a;
+    this->type = a;
 }
 
-int Attaque::getAttaque() const{
+// On récupère la valeur de l'attaque (0, 1 ou 2)
+int Attaque::getAttaque() const
+{
     return type;
 }
 
-bool Attaque::resoudreAttaque(Attaque &a) const {
-    ;
+// Vérification de l'attaque, renvoie true si l'attaque est réussie, sinon false
+bool Attaque::resoudreAttaque(Attaque &a) const
+{
+    if (this->type == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-string getNomAttaque() {
-    ;
+// On récupère le nom de l'attaque en fonction de la valeur du type
+string Attaque::getNomAttaque() const
+{
+    if (type == 0)
+    {
+        return "Pierre";
+    }
+    else if (type == 1)
+    {
+        return "Feuille";
+    }
+    else if (type == 2)
+    {
+        return "Ciseaux";
+    }
+    else
+    {
+        return 0;
+    }
 }
-
--- Attaque.h
+++ Attaque.h
@@ -1,26 +1,29 @@
 #ifndef __secsmp_attaque_h
 #define __secsmp_attaque_h
+
+// Bibliothèques
 #include <string>
 #include <iostream>
 #include "stdlib.h"
 
 using namespace std;
 
+// Classe attaque pour intéragir avec le plateau
 class Attaque
 {
 
 private:
-    int type;
+    int type; // Type d'attaque : 0 = pierre, 1 = feuille, 2 = ciseaux
 
 public:
     // Constructeurs
-    Attaque();
-    Attaque(int a);
+    Attaque();      // Création attaque random
+    Attaque(int a); // Création attaque spécifique
 
     // Accesseurs
-    int getAttaque() const;
-    bool resoudreAttaque(Attaque &a) const;
-    string getNomAttaque() const;
+    int getAttaque() const;                 // Récupération du type d'attaque
+    bool resoudreAttaque(Attaque &a) const; // true = Att. réussie, false = Att. échouée
+    string getNomAttaque() const;           // Récupération du nom d'attaque
     // Mutateurs
 };
 
